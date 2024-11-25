733ced7fd48e21bc99eba4d0da49109e61a4d55f
DvorsakGal <gal.dvorsak2002@gmail.com>
2024-11-25 19:47:48 +0100

    tretja naloga z dodanim weather apijem

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:intl/intl.dart';
 import 'package:weather_app/auth.dart';
+import 'package:weather/weather.dart';
+import 'package:weather_app/consts.dart';
 
-class HomePage extends StatelessWidget {
-  HomePage({Key? key}) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   final User? user = Auth().currentUser;
 
   Future<void> signOut() async {
@@ -26,9 +34,226 @@ class HomePage extends StatelessWidget {
     );
   }
 
+  // WEATHER VARIABLES:
+  final WeatherFactory _weatherFactory = WeatherFactory(OPENWEATHER_API_KEY);
+
+  Weather? _weather;
+
+  @override
+  void initState() {
+    super.initState();
+    _weatherFactory.currentWeatherByCityName("Buenos Aires").then((weather) {
+      setState(() {
+        _weather = weather;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: _buildUI(),
+    );
+  }
+
+  Widget _buildUI() {
+    if (_weather == null) {
+      return Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _userUid(),
+            _signOutButton(),
+            const CircularProgressIndicator(),
+          ],
+        ),
+      );
+    }
+    return SizedBox(
+      width: MediaQuery.sizeOf(context).width,
+      height: MediaQuery.sizeOf(context).height,
+      child: Column(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.04,
+          ),
+          _userUid(),
+          _signOutButton(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.06,
+          ),
+          _locationHeader(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.06,
+          ),
+          _dateTimeInfo(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.05,
+          ),
+          _weatherIcon(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.02,
+          ),
+          _currentTemp(),
+          SizedBox(
+            height: MediaQuery.sizeOf(context).height * 0.02,
+          ),
+          _extraInfo(),
+        ],
+      ),
+    );
+  }
+
+  Widget _locationHeader() {
+    return Text(
+      _weather?.areaName ?? "",
+      style: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Widget _dateTimeInfo() {
+    DateTime now = _weather!.date!;
+    return Column(
+      children: [
+        Text(
+          DateFormat("h:mm a").format(now),
+          style: const TextStyle(
+            fontSize: 35,
+          ),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              DateFormat("EEEE").format(now),
+              style: const TextStyle(
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            Text(
+              "  ${DateFormat("d.M.y").format(now)}",
+              style: const TextStyle(
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        )
+      ],
+    );
+  }
+
+  Widget _weatherIcon() {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Container(
+          height: MediaQuery.sizeOf(context).height * 0.2,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: NetworkImage(
+                    "https://openweathermap.org/img/wn/${_weather?.weatherIcon}@4x.png")),
+          ),
+        ),
+        Text(
+          _weather?.weatherDescription ?? "",
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _currentTemp() {
+    return Text(
+      "${_weather?.temperature?.celsius?.toStringAsFixed(0)}°C",
+      style: const TextStyle(
+        color: Colors.black,
+        fontSize: 80,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Widget _extraInfo() {
+    return Container(
+      height: MediaQuery.sizeOf(context).height * 0.15,
+      width: MediaQuery.sizeOf(context).width * 0.80,
+      decoration: BoxDecoration(
+        color: Colors.deepPurpleAccent,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Max: ${_weather?.tempMax?.celsius?.toStringAsFixed(0)}°C",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+              Text(
+                "Min: ${_weather?.tempMin?.celsius?.toStringAsFixed(0)}°C",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              )
+            ],
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text(
+                "Wind: ${_weather?.windSpeed?.toStringAsFixed(0)} m/s",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              ),
+              Text(
+                "Humidity: ${_weather?.humidity?.toStringAsFixed(0)} %",
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 15,
+                ),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+/**
+ scaffold po koncanem zgolj auth functionalityju
+return Scaffold(
       appBar: AppBar(
         title: _title(),
       ),
@@ -46,5 +271,4 @@ class HomePage extends StatelessWidget {
         ),
       ),
     );
-  }
-}
+ */
